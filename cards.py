1a61104721904fb909e58dd016fa7d91fe402654
Benjamin Paothatat <bpaothat@gmail.com>
2022-06-19 08:35:16 -0700

    Add card class

---
+++ cards.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+from enum import Enum
+
+class Suit(Enum):
+    SPADE = 0
+    HEART = 1
+    DIAMOND = 2
+    CLUB = 3
+
+class Rank(Enum):
+    ACE = 1
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+    FIVE = 5
+    SIX = 6
+    SEVEN = 7
+    EIGHT = 8
+    NINE = 9
+    TEN = 10
+    JACK = 11
+    QUEEN = 12
+    KING = 13
+
+@dataclass
+class Card:
+    suit: Suit
+    rank: Rank
