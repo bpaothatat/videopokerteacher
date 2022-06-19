1e16fd90b6aa37c24ffa148d8e28d65e473670fe
Benjamin Paothatat <bpaothat@gmail.com>
2022-06-19 08:35:42 -0700

    Add deck class

---
+++ deck.py
@@ -0,0 +1,33 @@
+from cards import Card, Suit, Rank
+from random import randrange
+from typing import List
+
+class Deck:
+    def __init__(self) -> None:
+        self.deck = [Card(suit, rank) for suit in Suit for rank in Rank]
+        self.dealt = []
+
+    def __str__(self) -> str:
+        return str(self.deck)
+
+    def __len__(self) -> int:
+        return len(self.deck)
+
+    def deal_hand(self) -> list:  
+        return [self.deal_card() for i in range(5)]
+
+    def redeal_hand(self, hand:List[Card], kept:List[bool]):
+        for i in range(5):
+            if not kept[i]:
+                hand[i] = self.deal_card()
+        return hand
+
+    def deal_card(self) -> Card:
+        card = self.deck[randrange(52)]
+        while card in self.dealt:
+            card = self.deck[randrange(52)]
+        self.dealt.append(card)
+        return card
+
+    def reset(self) -> None:
+        self.dealt = []
