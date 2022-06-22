8b1cc6123cd623a84c7251244e6994919c1be846
Benjamin Paothatat <bpaothat@gmail.com>
2022-06-21 17:28:57 -0700

    Add straight validator test

--- deuces_wild_hand_validator_test.py
+++ deuces_wild_hand_validator_test.py
@@ -1,5 +1,5 @@
 from cards import Card, Suit, Rank
-from deuces_wild_hand_validator import deuce_flush
+from deuces_wild_hand_validator import deuce_flush, straight
 
 import unittest
 
@@ -17,6 +17,23 @@ class DeucesWildTest(unittest.TestCase):
         #Not a flush
         non_flush_hand = [Card(Suit.DIAMOND, Rank.ACE), Card(Suit.CLUB, Rank.KING), Card(Suit.CLUB, Rank.QUEEN), Card(Suit.CLUB, Rank.JACK), Card(Suit.CLUB, Rank.TEN)]
         self.assertFalse(deuce_flush(non_flush_hand))
+
+    def test_straight(self):
+        #Royal straight
+        royal_straight = [Card(Suit.DIAMOND, Rank.ACE), Card(Suit.CLUB, Rank.KING), Card(Suit.CLUB, Rank.QUEEN), Card(Suit.CLUB, Rank.JACK), Card(Suit.CLUB, Rank.TEN)]
+        self.assertTrue(straight(royal_straight))
+
+        #Random middle straight
+        random_middle_straight = [Card(Suit.DIAMOND, Rank.SIX), Card(Suit.CLUB, Rank.SEVEN), Card(Suit.CLUB, Rank.EIGHT), Card(Suit.CLUB, Rank.NINE), Card(Suit.CLUB, Rank.TEN)]
+        self.assertTrue(straight(random_middle_straight))
+
+        #Lower straight
+        lower_straight =  [Card(Suit.DIAMOND, Rank.ACE), Card(Suit.CLUB, Rank.TWO), Card(Suit.CLUB, Rank.THREE), Card(Suit.CLUB, Rank.FOUR), Card(Suit.CLUB, Rank.FIVE)]
+        self.assertTrue(straight(lower_straight))
+
+        #Non stright
+        non_straight = [Card(Suit.DIAMOND, Rank.ACE), Card(Suit.CLUB, Rank.TWO), Card(Suit.CLUB, Rank.THREE), Card(Suit.CLUB, Rank.SIX), Card(Suit.CLUB, Rank.FIVE)]
+        self.assertFalse(straight(non_straight))
 
 if __name__ == '__main__':
     unittest.main()
