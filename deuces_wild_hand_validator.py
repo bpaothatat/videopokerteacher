2fb8370c9d6c174e8868d6798efc99c5a4c48ab2
Benjamin Paothatat <bpaothat@gmail.com>
2022-06-21 17:28:37 -0700

    Add straight validator

--- deuces_wild_hand_validator.py
+++ deuces_wild_hand_validator.py
@@ -7,3 +7,17 @@ def flush(hand:List[Card]) -> bool:
 def deuce_flush(hand:List[Card]) -> bool:
     without_deuces = [card for card in hand if card.rank is not Rank.TWO]
     return flush(without_deuces)
+
+def straight(hand:List[Card]) -> bool:
+    sorted_hand = sorted(hand, key=lambda card: card.rank.value)
+    for i in range(4):
+        current_rank = sorted_hand[i].rank.value
+        next_rank = sorted_hand[i + 1].rank.value
+        if not next_straight(current_rank, next_rank):
+            return False
+    return True
+
+def next_straight(current_rank, next_rank):
+    king_ace = current_rank == 1 and next_rank == 10
+    greater_by_one = current_rank + 1 == next_rank
+    return king_ace or greater_by_one
