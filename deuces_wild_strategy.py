d6c60b18961ca64703696246610ce3f0eccd1d17
Benjamin Paothatat <bpaothat@gmail.com>
2022-06-27 19:29:38 -0700

    Add 3 deuces strategy check

--- deuces_wild_strategy.py
+++ deuces_wild_strategy.py
@@ -2,13 +2,23 @@ from cards import Card, Rank
 from deuces_wild_hand_validator import *
 
 def get_correct_hold_strategy(hand:List[Card]):
+    keep_all = [True] * 5
     result = [False] * 5
-    if two_counter(hand) == 4:
+    twos = two_counter(hand)
+    if twos == 4:
         result = keep_four_deuces(hand)
+    if twos == 3:
+        if royal_flush(hand) or five_of_a_kind(hand):
+            result = keep_all
+        else:
+            result = keep_deuces(hand)
     return result
 
 def two_counter(hand:List[Card]):
     return len([card for card in hand if card.rank == Rank.TWO])
+
+def keep_deuces(hand:List[Card]):
+    return [True if card.rank == Rank.TWO else None for card in hand]
 
 def keep_four_deuces(hand:List[Card]):
     return [True if card.rank == Rank.TWO else None for card in hand]
