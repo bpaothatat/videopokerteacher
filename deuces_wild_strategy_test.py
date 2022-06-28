c7135e131155df47519d9882fe86e8650b207a34
Benjamin Paothatat <bpaothat@gmail.com>
2022-06-27 19:29:50 -0700

    Add test for three deuces check

--- deuces_wild_strategy_test.py
+++ deuces_wild_strategy_test.py
@@ -5,10 +5,19 @@ import unittest
 
 class DeucesWildTest(unittest.TestCase):
     def test_four_deuces(self):
-        #Four twos
         four_twos = [Card(Suit.DIAMOND, Rank.TWO), Card(Suit.CLUB, Rank.TWO), Card(Suit.SPADE, Rank.TWO), Card(Suit.HEART, Rank.TWO), Card(Suit.CLUB, Rank.FIVE)]
         self.assertTrue(validate_strategy([True, True, True, True, False], get_correct_hold_strategy(four_twos)))
+        self.assertTrue(validate_strategy([True, True, True, True, True], get_correct_hold_strategy(four_twos)))
 
+    def test_three_deuces(self):
+        three_twos_wild_flush = [Card(Suit.DIAMOND, Rank.TWO), Card(Suit.CLUB, Rank.TWO), Card(Suit.SPADE, Rank.TWO), Card(Suit.HEART, Rank.ACE), Card(Suit.HEART, Rank.KING)]
+        self.assertTrue(validate_strategy([True, True, True, True, True], get_correct_hold_strategy(three_twos_wild_flush)))
+        
+        three_twos_five_of_a_kind = [Card(Suit.DIAMOND, Rank.TWO), Card(Suit.CLUB, Rank.TWO), Card(Suit.SPADE, Rank.TWO), Card(Suit.HEART, Rank.ACE), Card(Suit.SPADE, Rank.ACE)]
+        self.assertTrue(validate_strategy([True, True, True, True, True], get_correct_hold_strategy(three_twos_five_of_a_kind)))
+
+        three_twos_with_nothing = [Card(Suit.DIAMOND, Rank.TWO), Card(Suit.CLUB, Rank.TWO), Card(Suit.SPADE, Rank.TWO), Card(Suit.HEART, Rank.ACE), Card(Suit.SPADE, Rank.TWO)]
+        self.assertTrue(validate_strategy([True, True, True, True, True], get_correct_hold_strategy(three_twos_with_nothing)))
 
     def test_discard_all(self):
         four_twos = [Card(Suit.DIAMOND, Rank.ACE), Card(Suit.CLUB, Rank.SIX), Card(Suit.SPADE, Rank.FIVE), Card(Suit.HEART, Rank.TEN), Card(Suit.CLUB, Rank.JACK)]
