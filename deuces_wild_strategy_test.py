7c6e56997ce3a67b2e8b3532a2164ed65d5fdd77
Benjamin Paothatat <bpaothat@gmail.com>
2022-06-28 19:17:40 -0700

    Add strategy for two deuces test

--- deuces_wild_strategy_test.py
+++ deuces_wild_strategy_test.py
@@ -16,8 +16,25 @@ class DeucesWildTest(unittest.TestCase):
         three_twos_five_of_a_kind = [Card(Suit.DIAMOND, Rank.TWO), Card(Suit.CLUB, Rank.TWO), Card(Suit.SPADE, Rank.TWO), Card(Suit.HEART, Rank.ACE), Card(Suit.SPADE, Rank.ACE)]
         self.assertTrue(validate_strategy([True, True, True, True, True], get_correct_hold_strategy(three_twos_five_of_a_kind)))
 
-        three_twos_with_nothing = [Card(Suit.DIAMOND, Rank.TWO), Card(Suit.CLUB, Rank.TWO), Card(Suit.SPADE, Rank.TWO), Card(Suit.HEART, Rank.ACE), Card(Suit.SPADE, Rank.TWO)]
-        self.assertTrue(validate_strategy([True, True, True, True, True], get_correct_hold_strategy(three_twos_with_nothing)))
+        three_twos_with_nothing = [Card(Suit.DIAMOND, Rank.TWO), Card(Suit.CLUB, Rank.TWO), Card(Suit.SPADE, Rank.TWO), Card(Suit.HEART, Rank.ACE), Card(Suit.DIAMOND, Rank.SEVEN)]
+        self.assertTrue(validate_strategy([True, True, True, False, False], get_correct_hold_strategy(three_twos_with_nothing)))
+
+    def test_two_deuces(self):
+        two_twos_wild_flush = [Card(Suit.DIAMOND, Rank.TWO), Card(Suit.CLUB, Rank.TWO), Card(Suit.HEART, Rank.TEN), Card(Suit.HEART, Rank.ACE), Card(Suit.HEART, Rank.KING)]
+        self.assertTrue(validate_strategy([True, True, True, True, True], get_correct_hold_strategy(two_twos_wild_flush)))
+        
+        two_twos_five_of_a_kind = [Card(Suit.DIAMOND, Rank.TWO), Card(Suit.CLUB, Rank.TWO), Card(Suit.DIAMOND, Rank.ACE), Card(Suit.HEART, Rank.ACE), Card(Suit.SPADE, Rank.ACE)]
+        self.assertTrue(validate_strategy([True, True, True, True, True], get_correct_hold_strategy(two_twos_five_of_a_kind)))
+
+        two_twos_stright_flush = [Card(Suit.DIAMOND, Rank.TWO), Card(Suit.CLUB, Rank.TWO), Card(Suit.DIAMOND, Rank.ACE), Card(Suit.DIAMOND, Rank.THREE), Card(Suit.DIAMOND, Rank.FOUR)]
+        self.assertTrue(validate_strategy([True, True, True, True, True], get_correct_hold_strategy(two_twos_stright_flush)))
+
+        two_twos_four_of_a_kind = [Card(Suit.DIAMOND, Rank.TWO), Card(Suit.CLUB, Rank.TWO), Card(Suit.DIAMOND, Rank.FIVE), Card(Suit.HEART, Rank.ACE), Card(Suit.SPADE, Rank.ACE)]
+        self.assertTrue(validate_strategy([True, True, False, True, True], get_correct_hold_strategy(two_twos_four_of_a_kind)))
+
+        twos_twos_with_nothing = [Card(Suit.DIAMOND, Rank.TWO), Card(Suit.CLUB, Rank.TWO), Card(Suit.SPADE, Rank.THREE), Card(Suit.HEART, Rank.TEN), Card(Suit.DIAMOND, Rank.SEVEN)]
+        self.assertTrue(validate_strategy([True, True, False, False, False], get_correct_hold_strategy(twos_twos_with_nothing)))
+
 
     def test_discard_all(self):
         four_twos = [Card(Suit.DIAMOND, Rank.ACE), Card(Suit.CLUB, Rank.SIX), Card(Suit.SPADE, Rank.FIVE), Card(Suit.HEART, Rank.TEN), Card(Suit.CLUB, Rank.JACK)]
