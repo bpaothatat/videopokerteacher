b8218925aa5f7fb36d3bc229b7376123f697adcc
Benjamin Paothatat <bpaothat@gmail.com>
2022-07-03 12:11:14 -0700

    Add additional logic for one deuce and no deuces

--- deuces_wild_strategy.py
+++ deuces_wild_strategy.py
@@ -1,3 +1,5 @@
+from pyclbr import readmodule
+import re
 from cards import Card, Rank
 from deuces_wild_hand_validator import *
 
@@ -8,12 +10,12 @@ def get_correct_hold_strategy(hand:List[Card]):
     if twos == 4:
         result = keep_four_deuces(hand)
     elif twos == 3:
-        if royal_flush(hand) or five_of_a_kind(hand):
+        if royal_flush_with_deuces(hand) or five_of_a_kind(hand):
             result = keep_all
         else:
             result = keep_deuces(hand)
     elif twos == 2:
-        if royal_flush(hand) or five_of_a_kind(hand) or straight_flush(hand):
+        if royal_flush_with_deuces(hand) or five_of_a_kind(hand) or straight_flush(hand):
             result = keep_all
         elif four_of_a_kind(hand):
             result = keep_four_of_a_kind(hand)
@@ -23,28 +25,44 @@ def get_correct_hold_strategy(hand:List[Card]):
         else:
             result = keep_deuces(hand)
     elif twos == 1:
-        if  royal_flush(hand) or five_of_a_kind(hand) or straight_flush(hand):
+        if  royal_flush_with_deuces(hand) or five_of_a_kind(hand) or straight_flush(hand):
             result = keep_all
         elif four_of_a_kind(hand):
             result = keep_four_of_a_kind(hand)
         elif four_to_royal_flush(hand, twos):
             result = keep_four_to_royal_flush(hand)
+        elif full_house(hand):
+            result = keep_all
+        elif three_of_a_kind(hand):
+            result = keep_three_of_a_kind(hand)
+        elif straight(hand) or flush(hand):
+            result = keep_all
         else:
             result = keep_deuces(hand)
     else:
-        if royal_flush_without_deuces(hand) or straight_flush(hand):
+        if royal_flush_without_deuces(hand):
             result = keep_all
         elif four_to_royal_flush(hand, twos):
             result = keep_four_to_royal_flush(hand)
+        elif straight_flush(hand):
+            result = keep_all
+        elif four_of_a_kind(hand):
+            result = keep_four_of_a_kind(hand)
+        elif full_house(hand):
+            result = keep_all
+        elif three_of_a_kind(hand):
+            result = keep_three_of_a_kind(hand)
+        elif straight(hand) or flush(hand):
+            result = keep_all
     return result
 
-def keep_deuces(hand:List[Card]):
+def keep_deuces(hand:List[Card])-> List[Card]:
     return [True if card.rank == Rank.TWO else None for card in hand]
 
-def keep_four_deuces(hand:List[Card]):
+def keep_four_deuces(hand:List[Card])-> List[Card]:
     return [True if card.rank == Rank.TWO else None for card in hand]
 
-def keep_four_of_a_kind(hand:List[Card]):
+def keep_four_of_a_kind(hand:List[Card]) -> List[Card]:
     """
     Used only for hands with 2 or less threes
     """
@@ -56,11 +74,10 @@ def keep_four_of_a_kind(hand:List[Card]):
             rank = card_rank
     return [True if card.rank == Rank.TWO or card.rank == rank else False for card in hand]
 
-def four_to_royal_flush(hand:List[Card], two_count:int):
+def four_to_royal_flush(hand:List[Card], two_count:int) -> bool:
     result = False
     suit = None
-    update_hand = hand_without_twos(hand)
-    suit_counts = suit_count(update_hand)
+    suit_counts = suit_count(hand_without_twos(hand))
     for acutal_suit, count in suit_counts.items():
         if count + two_count is 4:
             suit = acutal_suit
@@ -68,8 +85,15 @@ def four_to_royal_flush(hand:List[Card], two_count:int):
         result = len([card for card in hand if card.rank.value > Rank.NINE.value]) + two_count == 4
     return result
 
-def keep_four_to_royal_flush(hand:List[Card]):
+def keep_four_to_royal_flush(hand:List[Card]) -> List[Card]:
     return [True if card.rank.value > Rank.NINE.value or card.rank is Rank.TWO else False for card in hand]
+
+def keep_three_of_a_kind(hand:List[Card], two_count:int) -> List[Card]:
+    rank_counts = rank_count(hand_without_twos(hand))
+    for actual_rank, count in rank_counts.items():
+        if count + two_count is 3:
+            result = [True if card.rank is Rank.TWO or card.rank is actual_rank else False for card in hand]
+    return result
 
 def validate_strategy(held:List[bool], expected:List[bool]) -> bool:
     result = True
--- deuces_wild_strategy_test.py
+++ deuces_wild_strategy_test.py
@@ -1,5 +1,6 @@
 from cards import Card, Suit, Rank
-from deuces_wild_strategy import get_correct_hold_strategy, validate_strategy
+from deuces_wild_hand_validator import *
+from deuces_wild_strategy import *
 
 import unittest
 
@@ -11,68 +12,96 @@ class DeucesWildTest(unittest.TestCase):
 
     def test_three_deuces(self):
         three_twos_wild_flush = [Card(Suit.DIAMOND, Rank.TWO), Card(Suit.CLUB, Rank.TWO), Card(Suit.SPADE, Rank.TWO), Card(Suit.HEART, Rank.ACE), Card(Suit.HEART, Rank.KING)]
-        self.assertTrue(validate_strategy([True, True, True, True, True], get_correct_hold_strategy(three_twos_wild_flush)))
+        self.assertEqual([True, True, True, True, True], get_correct_hold_strategy(three_twos_wild_flush))
         
         three_twos_five_of_a_kind = [Card(Suit.DIAMOND, Rank.TWO), Card(Suit.CLUB, Rank.TWO), Card(Suit.SPADE, Rank.TWO), Card(Suit.HEART, Rank.ACE), Card(Suit.SPADE, Rank.ACE)]
-        self.assertTrue(validate_strategy([True, True, True, True, True], get_correct_hold_strategy(three_twos_five_of_a_kind)))
+        self.assertEqual([True, True, True, True, True], get_correct_hold_strategy(three_twos_five_of_a_kind))
 
         three_twos_with_nothing = [Card(Suit.DIAMOND, Rank.TWO), Card(Suit.CLUB, Rank.TWO), Card(Suit.SPADE, Rank.TWO), Card(Suit.HEART, Rank.ACE), Card(Suit.DIAMOND, Rank.SEVEN)]
-        self.assertTrue(validate_strategy([True, True, True, False, False], get_correct_hold_strategy(three_twos_with_nothing)))
+        self.assertEqual([True, True, True, False, False], get_correct_hold_strategy(three_twos_with_nothing))
 
     def test_two_deuces(self):
         two_twos_wild_flush = [Card(Suit.DIAMOND, Rank.TWO), Card(Suit.CLUB, Rank.TWO), Card(Suit.HEART, Rank.TEN), Card(Suit.HEART, Rank.ACE), Card(Suit.HEART, Rank.KING)]
-        self.assertTrue(validate_strategy([True, True, True, True, True], get_correct_hold_strategy(two_twos_wild_flush)))
+        self.assertEqual([True, True, True, True, True], get_correct_hold_strategy(two_twos_wild_flush))
         
         two_twos_five_of_a_kind = [Card(Suit.DIAMOND, Rank.TWO), Card(Suit.CLUB, Rank.TWO), Card(Suit.DIAMOND, Rank.ACE), Card(Suit.HEART, Rank.ACE), Card(Suit.SPADE, Rank.ACE)]
-        self.assertTrue(validate_strategy([True, True, True, True, True], get_correct_hold_strategy(two_twos_five_of_a_kind)))
+        self.assertEqual([True, True, True, True, True], get_correct_hold_strategy(two_twos_five_of_a_kind))
 
         two_twos_stright_flush = [Card(Suit.DIAMOND, Rank.TWO), Card(Suit.CLUB, Rank.TWO), Card(Suit.DIAMOND, Rank.ACE), Card(Suit.DIAMOND, Rank.THREE), Card(Suit.DIAMOND, Rank.FOUR)]
-        self.assertTrue(validate_strategy([True, True, True, True, True], get_correct_hold_strategy(two_twos_stright_flush)))
+        self.assertEqual([True, True, True, True, True], get_correct_hold_strategy(two_twos_stright_flush))
 
         two_twos_four_of_a_kind = [Card(Suit.DIAMOND, Rank.TWO), Card(Suit.CLUB, Rank.TWO), Card(Suit.DIAMOND, Rank.FIVE), Card(Suit.HEART, Rank.ACE), Card(Suit.SPADE, Rank.ACE)]
-        self.assertTrue(validate_strategy([True, True, False, True, True], get_correct_hold_strategy(two_twos_four_of_a_kind)))
+        self.assertEqual([True, True, False, True, True], get_correct_hold_strategy(two_twos_four_of_a_kind))
 
         near_to_royal_flush = [Card(Suit.HEART, Rank.SIX), Card(Suit.HEART, Rank.TWO), Card(Suit.DIAMOND, Rank.TWO), Card(Suit.HEART, Rank.JACK), Card(Suit.HEART, Rank.TEN)]
-        self.assertTrue(validate_strategy([False, True, True, True, True], get_correct_hold_strategy(near_to_royal_flush)))
+        self.assertEqual([False, True, True, True, True], get_correct_hold_strategy(near_to_royal_flush))
         
         twos_twos_with_nothing = [Card(Suit.DIAMOND, Rank.TWO), Card(Suit.CLUB, Rank.TWO), Card(Suit.SPADE, Rank.THREE), Card(Suit.HEART, Rank.TEN), Card(Suit.DIAMOND, Rank.SEVEN)]
-        self.assertTrue(validate_strategy([True, True, False, False, False], get_correct_hold_strategy(twos_twos_with_nothing)))
+        self.assertEqual([True, True, False, False, False], get_correct_hold_strategy(twos_twos_with_nothing))
 
     def test_one_deuces(self):
         one_two_wild_flush = [Card(Suit.DIAMOND, Rank.TWO), Card(Suit.HEART, Rank.QUEEN), Card(Suit.HEART, Rank.TEN), Card(Suit.HEART, Rank.ACE), Card(Suit.HEART, Rank.KING)]
-        self.assertTrue(validate_strategy([True, True, True, True, True], get_correct_hold_strategy(one_two_wild_flush)))
+        self.assertEqual([True, True, True, True, True], get_correct_hold_strategy(one_two_wild_flush))
         
         one_two_five_of_a_kind = [Card(Suit.DIAMOND, Rank.TWO), Card(Suit.CLUB, Rank.ACE), Card(Suit.DIAMOND, Rank.ACE), Card(Suit.HEART, Rank.ACE), Card(Suit.SPADE, Rank.ACE)]
-        self.assertTrue(validate_strategy([True, True, True, True, True], get_correct_hold_strategy(one_two_five_of_a_kind)))
+        self.assertEqual([True, True, True, True, True], get_correct_hold_strategy(one_two_five_of_a_kind))
 
         one_two_stright_flush = [Card(Suit.DIAMOND, Rank.FIVE), Card(Suit.CLUB, Rank.TWO), Card(Suit.DIAMOND, Rank.ACE), Card(Suit.DIAMOND, Rank.THREE), Card(Suit.DIAMOND, Rank.FOUR)]
-        self.assertTrue(validate_strategy([True, True, True, True, True], get_correct_hold_strategy(one_two_stright_flush)))
+        self.assertEqual([True, True, True, True, True], get_correct_hold_strategy(one_two_stright_flush))
 
         one_two_four_of_a_kind = [Card(Suit.DIAMOND, Rank.ACE), Card(Suit.CLUB, Rank.TWO), Card(Suit.DIAMOND, Rank.FIVE), Card(Suit.HEART, Rank.ACE), Card(Suit.SPADE, Rank.ACE)]
-        self.assertTrue(validate_strategy([True, True, False, True, True], get_correct_hold_strategy(one_two_four_of_a_kind)))
+        self.assertEqual([True, True, False, True, True], get_correct_hold_strategy(one_two_four_of_a_kind))
 
         near_to_royal_flush = [Card(Suit.HEART, Rank.SIX), Card(Suit.HEART, Rank.TWO), Card(Suit.HEART, Rank.QUEEN), Card(Suit.HEART, Rank.JACK), Card(Suit.HEART, Rank.TEN)]
-        self.assertTrue(validate_strategy([False, True, True, True, True], get_correct_hold_strategy(near_to_royal_flush)))
+        self.assertEqual([False, True, True, True, True], get_correct_hold_strategy(near_to_royal_flush))
+
+        full_house_test = [Card(Suit.HEART, Rank.TWO), Card(Suit.DIAMOND, Rank.ACE), Card(Suit.SPADE, Rank.THREE), Card(Suit.CLUB, Rank.ACE), Card(Suit.CLUB, Rank.THREE)]
+        self.assertEqual([True, True, True, True, True], get_correct_hold_strategy(full_house_test))
+
+        three_of_a_kind_test = [Card(Suit.HEART, Rank.ACE), Card(Suit.DIAMOND, Rank.ACE), Card(Suit.SPADE, Rank.FIVE), Card(Suit.CLUB, Rank.TWO), Card(Suit.CLUB, Rank.THREE)]
+        self.assertEqual([True, True, False, True, False], get_correct_hold_strategy(three_of_a_kind_test))
+
+        straight_test = [Card(Suit.SPADE, Rank.TWO), Card(Suit.DIAMOND, Rank.SIX), Card(Suit.SPADE, Rank.FIVE), Card(Suit.CLUB, Rank.FOUR), Card(Suit.CLUB, Rank.THREE)]
+        self.assertEqual([True, True, True, True, True], get_correct_hold_strategy(straight_test))
+
+        flush_test = [Card(Suit.HEART, Rank.TEN), Card(Suit.HEART, Rank.SIX), Card(Suit.HEART, Rank.JACK), Card(Suit.HEART, Rank.FOUR), Card(Suit.SPADE, Rank.TWO)]
+        self.assertEqual([True, True, True, True, True], get_correct_hold_strategy(flush_test))
+
 
         one_two_with_nothing = [Card(Suit.DIAMOND, Rank.TWO), Card(Suit.CLUB, Rank.THREE), Card(Suit.SPADE, Rank.SIX), Card(Suit.HEART, Rank.TEN), Card(Suit.DIAMOND, Rank.SEVEN)]
-        self.assertTrue(validate_strategy([True, False, False, False, False], get_correct_hold_strategy(one_two_with_nothing)))
+        self.assertEqual([True, False, False, False, False], get_correct_hold_strategy(one_two_with_nothing))
 
     def test_no_deuces(self):
         no_twos_royal_flush = [Card(Suit.HEART, Rank.JACK), Card(Suit.HEART, Rank.QUEEN), Card(Suit.HEART, Rank.TEN), Card(Suit.HEART, Rank.ACE), Card(Suit.HEART, Rank.KING)]
-        self.assertTrue(validate_strategy([True, True, True, True, True], get_correct_hold_strategy(no_twos_royal_flush)))
+        self.assertEqual([True, True, True, True, True], get_correct_hold_strategy(no_twos_royal_flush))
         
-        no_two_straight_flush = [Card(Suit.HEART, Rank.SIX), Card(Suit.HEART, Rank.SEVEN), Card(Suit.HEART, Rank.EIGHT), Card(Suit.HEART, Rank.NINE), Card(Suit.HEART, Rank.TEN)]
-        self.assertTrue(validate_strategy([True, True, True, True, True], get_correct_hold_strategy(no_two_straight_flush)))
-
         near_to_royal_flush = [Card(Suit.HEART, Rank.SIX), Card(Suit.HEART, Rank.KING), Card(Suit.HEART, Rank.QUEEN), Card(Suit.HEART, Rank.JACK), Card(Suit.HEART, Rank.TEN)]
-        self.assertTrue(validate_strategy([False, True, True, True, True], get_correct_hold_strategy(near_to_royal_flush)))
+        self.assertEqual([False, True, True, True, True], get_correct_hold_strategy(near_to_royal_flush))
 
-        one_two_with_nothing = [Card(Suit.DIAMOND, Rank.JACK), Card(Suit.CLUB, Rank.THREE), Card(Suit.SPADE, Rank.SIX), Card(Suit.HEART, Rank.TEN), Card(Suit.DIAMOND, Rank.SEVEN)]
-        self.assertTrue(validate_strategy([False, False, False, False, False], get_correct_hold_strategy(one_two_with_nothing)))
+        no_two_straight_flush = [Card(Suit.HEART, Rank.SIX), Card(Suit.HEART, Rank.SEVEN), Card(Suit.HEART, Rank.EIGHT), Card(Suit.HEART, Rank.NINE), Card(Suit.HEART, Rank.TEN)]
+        self.assertEqual([True, True, True, True, True], get_correct_hold_strategy(no_two_straight_flush))
+
+        four_of_a_kind_test = [Card(Suit.HEART, Rank.ACE), Card(Suit.DIAMOND, Rank.ACE), Card(Suit.SPADE, Rank.ACE), Card(Suit.CLUB, Rank.ACE), Card(Suit.CLUB, Rank.THREE)]
+        self.assertEqual([True, True, True, True, False], get_correct_hold_strategy(four_of_a_kind_test))
+
+        full_house_test = [Card(Suit.HEART, Rank.ACE), Card(Suit.DIAMOND, Rank.ACE), Card(Suit.SPADE, Rank.THREE), Card(Suit.CLUB, Rank.ACE), Card(Suit.CLUB, Rank.THREE)]
+        self.assertEqual([True, True, True, True, True], get_correct_hold_strategy(full_house_test))
+
+        three_of_a_kind_test = [Card(Suit.HEART, Rank.ACE), Card(Suit.DIAMOND, Rank.ACE), Card(Suit.SPADE, Rank.FIVE), Card(Suit.CLUB, Rank.ACE), Card(Suit.CLUB, Rank.THREE)]
+        self.assertEqual([True, True, False, True, False], get_correct_hold_strategy(three_of_a_kind_test))
+
+        straight_test = [Card(Suit.HEART, Rank.SEVEN), Card(Suit.DIAMOND, Rank.SIX), Card(Suit.SPADE, Rank.FIVE), Card(Suit.CLUB, Rank.FOUR), Card(Suit.CLUB, Rank.THREE)]
+        self.assertEqual([True, True, True, True, True], get_correct_hold_strategy(straight_test))
+
+        flush_test = [Card(Suit.HEART, Rank.TEN), Card(Suit.HEART, Rank.SIX), Card(Suit.HEART, Rank.JACK), Card(Suit.HEART, Rank.FOUR), Card(Suit.HEART, Rank.THREE)]
+        self.assertEqual([True, True, True, True, True], get_correct_hold_strategy(flush_test))
+
+        nothing_test = [Card(Suit.DIAMOND, Rank.JACK), Card(Suit.CLUB, Rank.THREE), Card(Suit.SPADE, Rank.SIX), Card(Suit.HEART, Rank.TEN), Card(Suit.DIAMOND, Rank.SEVEN)]
+        self.assertEqual([False, False, False, False, False], get_correct_hold_strategy(nothing_test))
 
     def test_discard_all(self):
         four_twos = [Card(Suit.DIAMOND, Rank.ACE), Card(Suit.CLUB, Rank.SIX), Card(Suit.SPADE, Rank.FIVE), Card(Suit.HEART, Rank.TEN), Card(Suit.CLUB, Rank.JACK)]
-        self.assertTrue(validate_strategy([False, False, False, False, False], get_correct_hold_strategy(four_twos)))
+        self.assertEqual([False, False, False, False, False], get_correct_hold_strategy(four_twos))
 
 if __name__ == '__main__':
     unittest.main()
