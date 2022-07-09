4283fbbf41ef0119a1609082ad5f5370772e7332
Benjamin Paothatat <bpaothat@gmail.com>
2022-07-09 16:46:11 -0700

    Initial gui

---
+++ gui.py
@@ -0,0 +1,44 @@
+from tkinter import *
+
+root = Tk()
+root.title("Deuces Wild")
+root.geometry("1200x800")
+root.configure(background="green")
+
+game_frame = Frame(root, bg = "green")
+game_frame.pack(pady=20)
+
+first_card_frame = LabelFrame(game_frame, text="First Card", bd=0)
+first_card_frame.grid(row=0, column=0, padx=10, ipadx=10)
+
+first_card = Label(first_card_frame, text='Two of Clubs')
+first_card.pack(padx=20)
+
+second_card_frame = LabelFrame(game_frame, text="Second Card", bd=0)
+second_card_frame.grid(row=0, column=1, padx=10, ipadx=10)
+
+second_card = Label(second_card_frame, text='Ace of Diamonds')
+second_card.pack(padx=20)
+
+third_card_frame = LabelFrame(game_frame, text="Third Card", bd=0)
+third_card_frame.grid(row=0, column=2, padx=10, ipadx=10)
+
+third_card = Label(third_card_frame, text='Two of Diamonds')
+third_card.pack(padx=20)
+
+fourth_card_frame = LabelFrame(game_frame, text="Fourth Card", bd=0)
+fourth_card_frame.grid(row=0, column=3, padx=10, ipadx=10)
+
+fourth_card = Label(fourth_card_frame, text='Three of Diamonds')
+fourth_card.pack(padx=20)
+
+fifth_card_frame = LabelFrame(game_frame, text="Fifth Card", bd=0)
+fifth_card_frame.grid(row=0, column=4, padx=10, ipadx=10)
+
+fifth_card = Label(fifth_card_frame, text='Four of Diamonds')
+fifth_card.pack(padx=20)
+
+deal_button = Button(game_frame, text="Deal")
+deal_button.grid(row=1, column=2, padx=20, pady=20)
+
+root.mainloop()
