a9157f6da87742ea51ed528cd71ecf6dbe993715
Benjamin Paothatat <bpaothat@gmail.com>
2022-07-10 12:26:43 -0700

    Add toggle of buttons

--- gui.py
+++ gui.py
@@ -8,14 +8,13 @@ root.configure(background="green")
 game_frame = Frame(root, bg = "green")
 game_frame.pack(pady=20)
 
+new_hand = True
+
 first_card_frame = LabelFrame(game_frame, text="First Card", bd=0)
 first_card_frame.grid(row=0, column=0, padx=10, ipadx=10)
 
 first_card = Label(first_card_frame, text='Two of Clubs')
 first_card.pack(padx=20)
-
-first_card_hold = Button(game_frame, text='Hold')
-first_card_hold.grid(row=1, column=0, padx=10, ipadx=10, pady=10)
 
 second_card_frame = LabelFrame(game_frame, text="Second Card", bd=0)
 second_card_frame.grid(row=0, column=1, padx=10, ipadx=10)
@@ -23,17 +22,11 @@ second_card_frame.grid(row=0, column=1, padx=10, ipadx=10)
 second_card = Label(second_card_frame, text='Ace of Diamonds')
 second_card.pack(padx=20)
 
-second_card_hold = Button(game_frame, text='Hold')
-second_card_hold.grid(row=1, column=1, padx=10, ipadx=10, pady=10)
-
 third_card_frame = LabelFrame(game_frame, text="Third Card", bd=0)
 third_card_frame.grid(row=0, column=2, padx=10, ipadx=10)
 
 third_card = Label(third_card_frame, text='Two of Diamonds')
 third_card.pack(padx=20)
-
-third_card_hold = Button(game_frame, text='Hold')
-third_card_hold.grid(row=1, column=2, padx=10, ipadx=10, pady=10)
 
 fourth_card_frame = LabelFrame(game_frame, text="Fourth Card", bd=0)
 fourth_card_frame.grid(row=0, column=3, padx=10, ipadx=10)
@@ -41,19 +34,50 @@ fourth_card_frame.grid(row=0, column=3, padx=10, ipadx=10)
 fourth_card = Label(fourth_card_frame, text='Three of Diamonds')
 fourth_card.pack(padx=20)
 
-fourth_card_hold = Button(game_frame, text='Hold')
-fourth_card_hold.grid(row=1, column=3, padx=10, ipadx=10, pady=10)
-
 fifth_card_frame = LabelFrame(game_frame, text="Fifth Card", bd=0)
 fifth_card_frame.grid(row=0, column=4, padx=10, ipadx=10)
 
 fifth_card = Label(fifth_card_frame, text='Four of Diamonds')
 fifth_card.pack(padx=20)
 
+first_card_hold = Button(game_frame, text='Hold')
+first_card_hold.grid(row=1, column=0, padx=10, ipadx=10, pady=10)
+first_card_hold.grid_remove()
+
+second_card_hold = Button(game_frame, text='Hold')
+second_card_hold.grid(row=1, column=1, padx=10, ipadx=10, pady=10)
+second_card_hold.grid_remove()
+
+third_card_hold = Button(game_frame, text='Hold')
+third_card_hold.grid(row=1, column=2, padx=10, ipadx=10, pady=10)
+third_card_hold.grid_remove()
+
+fourth_card_hold = Button(game_frame, text='Hold')
+fourth_card_hold.grid(row=1, column=3, padx=10, ipadx=10, pady=10)
+fourth_card_hold.grid_remove()
+
 fifth_card_hold = Button(game_frame, text='Hold')
 fifth_card_hold.grid(row=1, column=4, padx=10, ipadx=10, pady=10)
+fifth_card_hold.grid_remove()
 
-deal_button = Button(game_frame, text="Deal")
+def deal():
+    global new_hand
+    if new_hand:
+        new_hand = False
+        first_card_hold.grid_remove()
+        second_card_hold.grid_remove()
+        third_card_hold.grid_remove()
+        fourth_card_hold.grid_remove()
+        fifth_card_hold.grid_remove()
+    else:
+        new_hand = True
+        first_card_hold.grid()
+        second_card_hold.grid()
+        third_card_hold.grid()
+        fourth_card_hold.grid()
+        fifth_card_hold.grid()
+        
+deal_button = Button(game_frame, text="Deal", command=deal)
 deal_button.grid(row=2, column=2, padx=20, pady=20)
 
 root.mainloop()
